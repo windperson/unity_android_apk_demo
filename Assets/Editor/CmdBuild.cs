6f837ac461265591a52792edea86a60ac4341d1c
windperson <windperson@gmail.com>
2016-05-30 16:57:33 +0800

    can build apk from command line -executeMethod "Namespace.ClassName.StaticMethod"

---
+++ Assets/Editor/CmdBuild.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Editor
+{
+    public class CmdBuild
+    {
+        static void Android()
+        {
+            BuildPipeline.BuildPlayer(GetScenes(), "C:/temp/" + "demo.apk", BuildTarget.Android, BuildOptions.None);
+        }
+
+        static string[] GetScenes()
+        {
+            return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+        }
+    }
+}
